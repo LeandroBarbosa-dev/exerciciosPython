340e02395a7f7e7ec91780570a6daa3cd7ab085c
Leandro Barbosa <contato.barbosaads@gmail.com>
2026-02-28 23:04:52 -0300

    feat: resolvendo exercícios

--- ex044.py
+++ ex044.py
@@ -7,44 +7,39 @@ o seu PREÇO NORMAL e CONDIÇÃO DE PAGAMENTO:
 > em até 2x no cartão: preço normal
 > 3x ou mais no cartão: 20% de juros
 """
-from time import sleep
+print('{:=^40}'.format(' LOJAS BARBOSA '))
 
+preco = float(input('Digite o preço do produto: R$ '))
 
-while True:
-    print('-' * 45)
-    print('       Escolha a forma de pagamento:')
-    print('[1] à vista dinheiro/cheque: 10% de desconto')
-    print('[2] à vista no cartão: 5% de desconto')
-    print('[3] até 2x no cartão: preço normal')
-    print('[4] 3x ou mais no cartão: 20% de juros')
-    print('[5] Sair')
-    print('-' * 45)
-    opcao = int(input('Opção: '))
+print('Escolha a forma de pagamento:')
+print('[ 1 ] à vista dinheiro/cheque')
+print('[ 2 ] à vista no cartão')
+print('[ 3 ] até 2x no cartão')
+print('[ 4 ] 3x ou mais no cartão')
+print('-' * 45)
+opcao = int(input('Opção: '))
 
+if opcao == 1:
+    precoFinal = preco - (preco * (10 / 100))
+    print(f'Total a pagar: R${precoFinal:.2f}\n')
 
-    if opcao == 1:
-        preco = float(input('Digite o preço do produto: R$ '))
-        precoFinal = preco - (preco * (10 / 100))
-        print(f'Total a pagar: R${precoFinal:.2f}')
+elif opcao == 2:
+    precoFinal = preco - (preco * (5 / 100))
+    print(f'Total a pagar: R${precoFinal:.2f}\n')
 
-    elif opcao == 2:
-        preco = float(input('Digite o preço do produto: R$ '))
-        precoFinal = preco - (preco * (5 / 100))
-        print(f'Total a pagar: R${precoFinal:.2f}')
+elif opcao == 3:
+    parcela = int(input('Quantas parcelas: '))
+    preco_parcela = preco / parcela
+    print(f'Total a pagar: R${preco:.2f}')
+    print(f'Sua comprada parcelada em {parcela}x de R${preco_parcela:.2f}.\n')
 
-    elif opcao == 3:
-        preco = float(input('Digite o preço do produto: R$ '))
-        print(f'Total a pagar: R${preco:.2f} \nTotal por parcela R${preco / 2:.2f}')
+elif opcao == 4:
+    parcela = int(input('Quantas parcelas: '))
+    precoFinal = preco + (preco * (20 / 100))
+    preco_parcela = precoFinal / parcela
+    print(f'Sua compra parcelada em {parcela}x de R${preco_parcela:.2f}')
+    print(f'Sua compra de R${preco:.2f} vai custar R${precoFinal:.2f} no final. COM JUROS\n')
 
-    elif opcao == 4:
-        preco = float(input('Digite o preço do produto: R$ '))
-        precoFinal = preco + (preco * (20 / 100))
-        print(f'Total a pagar: R${precoFinal:.2f} \nTotal por parcela R${precoFinal / 3:.2f}')
-
-    elif opcao == 5:
-        print('Encerrando...')
-        sleep(2)
-        break
-    else:
-        print('Opção inválida. Tente novamente!')
+else:
+    print('Opção inválida. Tente novamente!')
 
--- ex045.py
+++ ex045.py
@@ -1,4 +1,38 @@
 """
 Crie um programa que faça o computador jogar JAKENPÔ com você.
 """
+from random import randint
+from time import sleep
+
+lista = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+
+jogador = int(input('''Faça sua jogada:
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura
+opção: '''))
+
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ')
+sleep(1)
+
+if computador == 0:
+    if jogador == 0:
+        print(f'EMPATE')
+
+elif computador == 1:
+    if jogador == 1:
+        print(f'EMPATE')
+elif computador == 2:
+    if jogador == 2:
+        print(f'EMPATE')
+else:
+    print('Opção inválida. Tente novamente!')
+
+print(f'O joagador escolheu: {lista[jogador]}')
+print(f'O computador escolheu: {lista[computador]}')
 
